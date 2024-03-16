d39f6b6a8a81cb7811b01a4b8c10aff2663b5517
Onajite Kelvin <onajiteokeh@gmail.com>
2024-03-16 08:36:26 +0100

    story update

--- components/sections/Ourstory.js
+++ components/sections/Ourstory.js
@@ -7,9 +7,9 @@ const Ourstory = () => {
         <div className="bg-[#96b85d] text-white p-5">
           <h3 className="uppercase text-2xl font-medium">Our Story</h3>
           <div className="font-thin">
-            <p className="my-4">My friend's wedding was supposed to be a celebration of love, not a blind date orchestrated by fate, confetti, and a questionable glass of champagne. But there she was, Lydia, radiating sunshine in a bridesmaid's dress and a smile that could launch a thousand ships (or at least steal my heart, apparently).</p>
-            <p className="my-4">It was like someone rewound my brain to '17-year-old-first-crush' mode. Blue eyes that rivaled the summer sky, laugh lines etched from a lifetime of joy, and the kind of confidence that made me want to hide under the closest buffet table. It wasn't just butterflies in my stomach, it was a full-blown aviary taking flight.</p>
-            <p className="my-4">The rest of the night was a haze of stolen glances, nervous smiles, and a constant, nagging feeling that the cake was lopsided but my priorities were definitely not. Days later, the only thing more lopsided than that cake was my sleep schedule. Every time I closed my eyes, I saw Lydia's laugh, heard her voice humming along to the wedding band. I was officially smitten.</p>
+            <p className="my-4">June 2020 will always be a registered date in our lives cause that’s when our love story began.</p>
+            <p className="my-4">As a guest at my friend’s wedding I couldn’t help but notice this pretty caramel skin girl trying her best to make the bride comfortable, I had my eyes on her all through that event but didn’t get a chance to talk to her cause she was too busy to even notice me plus she had a confusing ring on her wedding finger. </p>
+            <p className="my-4"> Days after the wedding I was talking to my friend and asked after the pretty girl from her wedding, that’s when I figured she was single, my friend’s cousin and she was my namesake too. I was eager to meet her and my friend offered to help me out, few hours later I got the pretty girl’s number, we got talking and concluded on a date after a while. </p>
 
           </div>
         </div>
@@ -21,9 +21,9 @@ const Ourstory = () => {
         <div className="bg-[#f1c4ba] text-white p-5">
           <h3 className="uppercase text-2xl font-medium">Our Story</h3>
           <div className="font-thin">
-            <p className="my-4">So, I did what any self-respecting, lovestruck fool would do – I cornered my friend, the poor soul who birthed this entire mess. "Dude," I pleaded, "you gotta introduce me to your cousin. My heart's doing the Macarena every time I think about her."</p>
-            <p className="my-4">And thankfully, my friend is a saint. One awkward coffee date later, and boom, I was toast. Conversation flowed like the free wine at the wedding reception, with just as much potential for disaster. But instead of spilled merlot, we spilled our hopes, dreams, and embarrassingly relatable childhood stories. I found myself falling deeper with every sip of laughter, every shared secret.</p>
-            <p className="my-4">Now, here we are, about to celebrate our own "I dos," a testament to the fact that sometimes, love doesn't need a grand gesture, just a wobbly cake tower and a little nudge from destiny (and maybe a very understanding friend). So cheers to us, Onajite and Lydia, proof that the best love stories start with laughter, a touch of fate, and enough champagne to make you swear you saw the cake tilt sideways. Here's to forever, one lopsided adventure at a time.</p>
+            <p className="my-4"> Days after the wedding I was talking to my friend and asked after the pretty girl from her wedding, that’s when I figured she was single, my friend’s cousin and she was my namesake too. I was eager to meet her and my friend offered to help me out, few hours later I got the pretty girl’s number, we got talking and concluded on a date after a while. </p>
+            <p className="my-4"> We had several meetings and chats after then but this lady was refusing to give me a chance to shower her with all the tender love I had to give, after a while we stopped talking and we were out of each other’s lives but she was meant to be mine so we met after a year of not talking to each other at an old schoolmate’s wedding (her cousin literally talked her into attending). </p>
+            <p className="my-4"> We had our reservations knowing that we’ve been out of each others lives for a while, but the connection was still there. And yes two years and a few months after we’re saying yes to forever.</p>
 
           </div>
         </div>
